30981079aa6d17682cd8f4291e695f2b7876020f
Parra <JLPARRA@kpmg.com.br>
2021-03-11 15:22:29 -0300

    Inputs components

---
+++ src/components/inputs/Input.vue
@@ -0,0 +1,40 @@
+<template>
+    <input
+      :type="type" 
+      :placeholder="label"
+      :value="value" 
+      @input="$emit('update:value', $event.target.value)"/>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        value: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            required: true
+        },
+        label: {
+            type: String
+        }
+    }
+})
+</script>
+<style lang="scss">
+@import '../../assets/_style.scss';
+input {
+    width: 50vw;
+    border-radius: 10px;
+    height: 30px;
+    padding: 10px;
+    border: 2px solid $secondary;
+    margin-bottom: 20px;
+    &:focus {
+        outline: none;
+    }
+}
+</style>
---
+++ src/components/inputs/Select.vue
@@ -0,0 +1,35 @@
+<template>
+    <select 
+      :value="value"
+      @input="$emit('update:value', $event.target.value)" >
+        <option selected value="">Select a product</option>
+        <slot></slot>
+    </select>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    props: {
+        value: {
+            type: String
+        }
+    }
+});
+</script>
+
+<style lang="scss">
+@import '../../assets/_style.scss';
+select {
+    width: 50vw;
+    border-radius: 10px;
+    height: 55px;
+    padding: 10px;
+    border: 2px solid $secondary;
+    margin-bottom: 20px;
+    &:focus {
+        outline: none;
+    }
+}
+</style>
