61aa55472433f62a5cd8f40719ef466cdb78227a
Parra <JLPARRA@kpmg.com.br>
2021-03-11 16:48:16 -0300

    New component: Table

---
+++ src/components/Table.vue
@@ -0,0 +1,45 @@
+<template>
+    <table>
+        <tr>
+            <th>Code</th>
+            <th>Product</th>
+            <th>In stock</th>
+        </tr>
+        <tr>
+            <td>05465</td>
+            <td>Banana</td>
+            <td>50</td>
+        </tr>
+        <tr>
+            <td>05465</td>
+            <td>Banana</td>
+            <td>50</td>
+        </tr>
+    </table>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        headers: {
+            type: []
+        }
+    }
+});
+</script>
+
+<style lang="scss">
+@import "../assets/_style.scss";
+
+table {
+    margin-top: 30px;
+    border-collapse: collapse;
+    th {
+        background-color: $primary;
+        color: white;
+        padding: 13px 30px;
+    }
+}
+</style>
